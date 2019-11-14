f39dadee74081b882c22ae2e0eaa4ee7aebd96ba
SheKei <43881082+SheKei@users.noreply.github.com>
2019-11-14 20:34:59 +0000

    Added more images. Created INSERT statements for item table

---
+++ assets/mySQL/insert_item.sql
@@ -0,0 +1,107 @@
+item_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name 
+	buying_cost 
+	selling_price 
+	category 
+	quantity 
+	min_reorder_amount 
+	vegan 
+	vegetarian 
+	nut_free 
+	img_path 
+	item_status 
+	
+--1.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Brownie", 1.00, 2.00, "Cake", 100, 10, true, true, false, "assets\img\brownie.jpg", "Sale");
+
+--2.
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Chocolate Cake", 1.50, 3.00, "Cake", 150, 10, false, true, false, "assets/img/chocolate.jpg", "Sale");
+
+--3.
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Chocolate Cake VEGAN", 2.00, 3.50, "Cake", 100, 10, true, true, false, "assets/img/chocolateVegan.jpg", "Sale");
+
+--4.
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Coffee", 1.00, 1.50, "Drink", 200, 20, false, true, true, "assets/img/coffee.jpg", "Sale");
+
+--5.
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Espresso", 1.50, 2.50, "Drink", 200, 20, true, true, true, "assets/img/espresso.jpg", "Sale");
+
+--6.
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Frapuccino", 2.50, 4.50, "Drink", 100, 20, false, true, true, "assets/img/frappucino.jpg", "Sale");
+
+--7.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Fruit Cake VEGAN", 2.00, 3.50, "Cake", 100, 20, true, true, true, "assets/img/fruit.jpg", "Sale");
+
+--8.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Hot Chocolate", 1.00, 2.50, "Drink", 200, 40, true, true, false, "assets/img/hotChocolate.jpg", "Sale");
+
+--9.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Jasmine Tea", 1.50, 3.00, "Drink", 140, 20, true, true, true, "assets/img/jasmine.jpg", "Sale");
+
+--10.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Latte", 1.00, 2.50, "Drink", 150, 15, false, true, true, "assets/img/latte.jpg", "Sale");
+
+--11.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Pastel Candy Cake", 2.00, 4.50, "Cake", 150, 10, false, false, true, "assets/img/pastel.jpg", "Sale");
+
+--12.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Peppermint Tea", 2.00, 2.50, "Drink", 160, 20, true, true, true, "assets/img/peppermint.jpg", "Sale");
+
+--13.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Rainbow Cake", 2.00, 4.00, "Cake", 100, 10, false, true, true, "assets/img/rainbow.jpg", "Sale");
+
+--14.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Raspberry Cake", 1.50, 2.50, "Cake", 100, 20, false, true, true, "assets/img/raspberry.jpg", "Sale");
+
+--15.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Red Tea", 2.00, 2.50, "Drink", 120, 20, true, true, true, "assets/img/redTea.jpg", "Sale");
+
+--16.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Red Velvet Cake", 2.00, 3.50, "Drink", 200, 20, false, false, false, "assets/img/redVelvet.jpg", "Sale");
+
+--17.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Tea", 1.00, 1.50, "Drink", 200, 50, true, true, true, "assets/img/tea.jpg", "Sale");
+
+--18.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Lemon Pie", 3.00, 5.00, "Cake", 20, 10, false, false, true, "assets/img/lemon.jpg", "Withdrawn");
+
+--19.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Jelly Cake", 2.00, 4.50, "Cake", 20, 10, true, true, true, "assets/img/jelly.jpg", "Withdrawn");
+
+--20.
+
+INSERT INTO ISAD251_STong.TeaRoom_Item(name, buying_cost,selling_price,category,quantity,min_reorder_amount,vegan,vegetarian,nut_free,img_path,item_status)
+VALUES("Cheese Cake", 3.00, 5.50, "Cake", 40, 10, true, true, true, "assets/img/cheese.jpg", "Withdrawn");
