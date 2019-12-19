1abc7a59c7e269823d09f09d9ff5230b5ecb9a07
SheKei <43881082+SheKei@users.noreply.github.com>
2019-12-19 20:28:34 +0000

    Order confirmed page made. User will be taken to this once they press confirm order button

---
+++ public/user/orderConfirmed.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once  'user_nav.php';
+include_once '../header.php';
+
+
+
+?>
+<head>
+
+    <style>
+        #confirmed
+        {
+            padding: 20px;
+        }
+    </style>
+
+</head>
+
+<body>
+
+<div class="jumbotron" id="confirmed">
+
+        <h1> Your Order has been Confirmed! </h1>
+        <p>You may wish to leave this page or stay</p>
+
+</div>
+
+</body>
+<?php
+include_once '../footer.php';
+
+?>
