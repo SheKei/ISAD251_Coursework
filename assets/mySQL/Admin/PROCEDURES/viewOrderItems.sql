be6d93f78b5177d9be9ce1de91cf1c6d2ab6bd31
SheKei <43881082+SheKei@users.noreply.github.com>
2020-01-08 23:45:08 +0000

    Added procedure to view order items of an order id

---
+++ assets/mySQL/Admin/PROCEDURES/viewOrderItems.sql
@@ -0,0 +1,29 @@
+DELIMITER //
+CREATE PROCEDURE ISAD251_STong.Tea_Admin_Order_Items (IN orderNumber int(11))
+BEGIN
+SELECT
+
+tearoom_table_order.order_id,
+tearoom_table_order.table_number,
+tearoom_order.order_date,
+tearoom_order.order_status,
+tearoom_item.item_id,
+tearoom_item.name,
+tearoom_table_order.order_quantity,
+SUM(tearoom_table_order.order_quantity * tearoom_item.selling_price) AS total_Price
+FROM
+isad251_stong.tearoom_item,
+isad251_stong.tearoom_order,
+isad251_stong.tearoom_table_order
+WHERE
+tearoom_table_order.order_id = orderNumber
+AND
+tearoom_item.item_id = tearoom_table_order.item_id
+AND
+tearoom_order.order_id = tearoom_table_order.order_id
+AND
+tearoom_order.table_number = tearoom_table_order.table_number
+GROUP BY tearoom_table_order.item_id, tearoom_table_order.order_id  
+ORDER BY `tearoom_table_order`.`order_id`  DESC;
+END //
+DELIMITER ;
